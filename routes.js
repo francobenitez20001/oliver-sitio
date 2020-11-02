c2e02a58b3414054a3ea3ebe2694dcd2ea102bc5
Franco Benitez <fhbenitez@hcdn.gob.ar>
2020-11-02 15:31:49 -0300

    update

--- routes.js
+++
@@ -1,7 +0,0 @@
-const routes = module.exports = require('next-routes')()
-
-routes
-.add('home', '/', 'index')
-.add('productosMarca', '/productos/:slugMarca.:idMarca', 'productos')
-.add('productosCategoria', '/productos/:slugCategoria.:idCategoria', 'productos')
-.add('producto', '/producto/:slugProducto.:idSubProducto', 'producto')
